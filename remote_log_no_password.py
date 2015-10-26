9e20a606796bed49676558fd71e64ae8ad106b3f
bghimire <gbibek@users.noreply.github.com>
2015-10-26 18:21:49 -0500

    Automate ssh login without password
    This code automates the process required to make remote ssh login password less.

---
+++ remote_log_no_password.py
@@ -0,0 +1,37 @@
+import os.path
+import os
+from subprocess import Popen, PIPE, STDOUT
+
+
+HOME_PATH    = os.environ['HOME']
+TARGET_DIR   = '/.ssh'
+TARGET_FILE  = 'id_rsa.pub'
+USER_NAME    = "xxxxxx"
+HOST         = "xxxxXX"
+USER_HOST    = USER_NAME + "@" + HOST 
+PATH_SSH_DIR     = HOME_PATH + TARGET_DIR
+PATH_TARGET_FILE = PATH_SSH_DIR +"/" + TARGET_FILE 
+AUTHORIZE_FILE   = PATH_SSH_DIR + "/authorized_keys"
+
+if not os.path.exists(PATH_SSH_DIR):
+    os.makedirs(PATH_SSH_DIR)
+
+os.chdir(PATH_SSH_DIR)
+
+
+if not os.path.isfile(PATH_TARGET_FILE):
+    print "Generating key.."
+    os.popen("ssh-keygen -t rsa")
+
+
+""" I used stderr=PIPE because I didnot want any error to be shown in the terminal to I piped it and ignored it"""
+
+print "for loging into the server "
+ret = Popen(["ssh","-o","StrictHostKeyChecking=no", USER_HOST, "mkdir", "-p", PATH_SSH_DIR], stderr=PIPE)
+ret.communicate()
+
+
+print "for editing file"
+ret1 = Popen(["cat", PATH_TARGET_FILE], stdout=PIPE)
+ret2 = Popen(["ssh", USER_HOST,"cat", ">>", AUTHORIZE_FILE],stdin=ret1.stdout)
+ret2.communicate()
