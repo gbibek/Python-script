3d0fb431cb7f8b3f9026b55fbc87e8d76965f421
Bibek.Ghimire <Bibek.Ghimire@amd.com>
2015-11-24 17:21:27 -0600

    This commit is for making the scritp more robust by checking on remote authorized_keys if it already has id_rsa.pub file.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Python-script
 Helpful python script
+
+
+* If asked "Enter file in which to save the key ... " Just hit enter.
+* You must enter password for the remote server for the first time.
+
--- remote_log_no_password.py
+++ remote_log_no_password.py
@@ -2,7 +2,6 @@ import os.path
 import os
 from subprocess import Popen, PIPE, STDOUT
 
-
 HOME_PATH    = os.environ['HOME']
 TARGET_DIR   = '/.ssh'
 TARGET_FILE  = 'id_rsa.pub'
@@ -13,25 +12,48 @@ PATH_SSH_DIR     = HOME_PATH + TARGET_DIR
 PATH_TARGET_FILE = PATH_SSH_DIR +"/" + TARGET_FILE 
 AUTHORIZE_FILE   = PATH_SSH_DIR + "/authorized_keys"
 
+def compare_lines(cmp_str, lines):
+    for line in lines:
+        if cmp_str == line: 
+	    return 1
+    return 0
+
 if not os.path.exists(PATH_SSH_DIR):
     os.makedirs(PATH_SSH_DIR)
-
 os.chdir(PATH_SSH_DIR)
-
 
 if not os.path.isfile(PATH_TARGET_FILE):
     print "Generating key.."
     os.popen("ssh-keygen -t rsa")
 
-
 """ I used stderr=PIPE because I didnot want any error to be shown in the terminal to I piped it and ignored it"""
 
-print "for loging into the server "
-ret = Popen(["ssh","-o","StrictHostKeyChecking=no", USER_HOST, "mkdir", "-p", "~/.ssh"], stderr=PIPE)
+print "Starting ... "
+
+ret = Popen(["ssh","-o","StrictHostKeyChecking=no", USER_HOST, "mkdir", "~/.ssh"], stderr=PIPE)
 ret.communicate()
+if ret.returncode:
+   print "Directory .ssh already exists."
+else:
+   print "Creating .ssh directory"
+ 
+local_ret  = Popen(["cat", PATH_TARGET_FILE], stdout=PIPE)
+remote_ret = Popen(["ssh", USER_HOST,"cat ~/.ssh/authorized_keys"],stdout=PIPE,stderr=PIPE)
 
+remote_lines = remote_ret.stdout.read()  
+remote_ret.communicate()
 
-print "for editing file"
-ret1 = Popen(["cat", PATH_TARGET_FILE], stdout=PIPE)
-ret2 = Popen(["ssh", USER_HOST,"cat > ~/.ssh/authorized_keys"],stdin=ret1.stdout)
-ret2.communicate()
+if remote_ret.returncode:
+    print "There is no such file and authorized_key file in remote server."
+else:
+    print "There is already authorized_keys file in remote server."
+
+if remote_ret.returncode == 0  and  compare_lines(local_ret.stdout.read(), remote_lines):
+    print "The id_pub.rsa is already on the remote authorized_keys file."   
+else:
+    print "Appending id_rsa.pub file into authorized_keys file of remote server"
+    ret = Popen(["ssh", USER_HOST,"cat >> ~/.ssh/authorized_keys"],stdin=local_ret.stdout)
+    ret.communicate()
+ 
+local_ret.communicate()
+print "Done !!"
